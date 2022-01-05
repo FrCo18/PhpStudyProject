7f6a93b62fcffe4c422d8ea4f7ed3d90ca830dde
Nikita <nikita2015borisov@gmail.com>
2022-01-05 23:51:15 +0300

    save progress

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Resources\PostCollection;
+use App\Http\Resources\UserCollection;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get( '/users', function () {
-//    $test = DB::table('Users')->get()->all();
-    dd(dd(DB::select('select 1')));
-    return User::all();
+    return new UserCollection(User::all());
 } );
 
 
